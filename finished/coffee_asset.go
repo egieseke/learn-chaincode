85d2a70cf37fbb270afdd4a0c00a96ce94832fde
egieseke <eric@kudoz.me>
2016-12-11 20:21:09 -0500

    fix

--- finished/coffee_asset.go
+++ finished/coffee_asset.go
@@ -271,13 +271,13 @@ func (t *SimpleChaincode) testCreateCoffeeAsset(stub shim.ChaincodeStubInterface
 
 	// TODO this should be the name of the farmer
 	newAccountArgs[0] = fmt.Sprintf("%s", accountBytes)
-	newAccountArgsArray := []string(newAccountArgs)
+	newAccountArgsArray := newAccountArgs[:]
 
 	t.createAccount(stub, newAccountArgsArray)
 
 	// now create the coffee asset
 	newCoffeeAssetArgs[0] = fmt.Sprintf("%s", coffeeAssetBytes)
-	newCoffeeAssetArgsArray := []string(newCoffeeAssetArgs)
+	newCoffeeAssetArgsArray := newCoffeeAssetArgs[:]
 	return t.createCoffeeAsset(stub, newCoffeeAssetArgsArray)
 
 }
